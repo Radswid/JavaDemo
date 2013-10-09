8579aace6c218b9def002ea0539b41465255592e
Radswid <krupskig@gmail.com>
2013-10-09 21:52:18 +0200

    ww

---
+++ JavaDemo/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		Punkt punkt = new Punkt();
+		punkt.x =100;
+		punkt.y =200;
+		punkt.wyswietlDane();
+		punkt.wyswietlDane();
+	}
+
+}
---
+++ JavaDemo/src/Punkt.java
@@ -0,0 +1,22 @@
+
+public class Punkt 
+{
+	int x;
+	int y;
+	
+	int pobierzX()
+	{
+		return x;
+	}
+	
+	int pobierzY()
+	{
+		return y;
+	}
+	
+	void wyswietlDane()
+	{
+		System.out.println("Wspolrzedna x= " + x);
+		System.out.println("Wspolrzedna y= " + y);
+	}
+}
